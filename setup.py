a8d52fe60224ae1d40cb412d87a81a4f273e2e59
Ally Hume <a.hume@ed.ac.uk>
2019-05-21 11:22:59 +0100

    Added more to setup.

--- setup.py
+++ setup.py
@@ -1,9 +1,22 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+        
 setup(name='nmmso',
       version='0.1',
-      description='Multi-Modal Search with the Niching Migratory Multi-Swarm Optimiser',
-      url='http://TODO',
+      description='Multi-Modal Search with the Niching Migratory Multi-Swarm Optimizer',
+      long_description=readme(),
+      classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3 :: Only',
+        'Topic :: Scientific/Engineering',
+        'Operating System :: OS Independent',
+      ],
+      keywords='optimization, optimisation, optimizer, optimiser, multimodal, multi-modal, GA, genetic',
+      url='https://github.com/EPCCed/NMMSO/wiki/NMMSO',
       author='Ally Hume',
       author_email='a.hume@epcc.ed.ac.uk',
       license='MIT',
